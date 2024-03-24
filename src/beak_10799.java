965cf0392b480ce7c498443a6398cbeebda9b674
Jung-won-seok <ukksj0621@naver.com>
2024-03-24 22:14:16 +0900

    commit

---
+++ src/beak_10799.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class beak_10799 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<>();
+        String str = sc.nextLine();
+        int result = 0;
+        for(int i=0 ; i<str.length() ; i++){
+            if(str.charAt(i) == '(') {
+                stack.push((int) '(');
+            }else if(str.charAt(i) == ')'){
+                stack.pop();
+            }
+            if(i>0){
+                if(str.charAt(i-1) == '(' && str.charAt(i) == ')'){
+                    result += stack.size();
+                }else if (str.charAt(i-1) == ')' && str.charAt(i) == ')'){
+                    result++;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
