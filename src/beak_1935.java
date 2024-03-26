daa6db81a1a9f53c95914a7a03e4497c75e18b7f
Jung-won-seok <ukksj0621@naver.com>
2024-03-26 16:18:41 +0900

    commit

---
+++ src/beak_1935.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class beak_1935 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<>();
+        int N = sc.nextInt();
+        sc.nextLine();
+        String str = sc.nextLine();
+        int arr[] = new int[N];
+        for(int i=0 ; i<N ; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+    }
+}
