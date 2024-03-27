0f616629a9e48c864bec8fbad394271e5c2c6d7c
Jung-won-seok <ukksj0621@naver.com>
2024-03-27 23:06:11 +0900

    commit

---
+++ src/beak_10808.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class beak_10808 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        char arr[] = input.toCharArray();
+        int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, z = 0;
+        for(int i=0 ; i< arr.length ; i++){
+
+        }
+    }
+}
