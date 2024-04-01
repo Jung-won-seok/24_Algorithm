59847b219472ba688692c66a2f9b3ccb8716e5b1
Jung-won-seok <ukksj0621@naver.com>
2024-04-01 14:21:37 +0900

    commit

---
+++ src/practice/beak_1943_scanner.java
@@ -0,0 +1,30 @@
+package practice;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class beak_1943_scanner {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0 ; i<T ; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int d = gcd(a ,b);
+            bw.write((a*b/d) + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static int gcd(int a, int b) {
+        if(b==0)
+            return a;
+        return gcd(b, a%b);
+    }
+}
