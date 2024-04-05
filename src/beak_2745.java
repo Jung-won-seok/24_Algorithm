c1c49f3e2e5be84c9e514ae1d63802095cd4a1f8
Jung-won-seok <ukksj0621@naver.com>
2024-04-05 17:51:48 +0900

    진법 변환

---
+++ src/beak_2745.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class beak_2745 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String N = st.nextToken();
+        int B = Integer.parseInt(st.nextToken());
+        br.close();
+
+        int temp = 1;
+        int sum = 0;
+        //ZZZZZ -> 60466175
+        for (int i = N.length() - 1; i >= 0; i--) {
+            char C = N.charAt(i);
+
+            if ('A' <= C && 'Z' >= C) { // 10=A, 11=B,,,,
+                sum += (C - 'A' + 10) * temp; // C가 F(15)라 하면 ->
+            } else {
+                sum += (C - '0') * temp;
+            }
+            temp *= B;
+        }
+        System.out.println(sum);
+    }
+}
