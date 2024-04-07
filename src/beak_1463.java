1048d68c6283cb51326223371218f4f4da6df878
Jung-won-seok <ukksj0621@naver.com>
2024-04-07 13:08:21 +0900

    다이나믹 프로그래밍(dp)

--- src/beak_1463.java
+++ src/beak_1463.java
@@ -4,28 +4,27 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class beak_1463 {
-    public static int d[];
+    public static Integer[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int number = Integer.parseInt(br.readLine());
-        d = new int[number + 1];
-        System.out.println(calculate(number));
+        int N = Integer.parseInt(br.readLine());
+        dp = new Integer[N + 1];
+        dp[0] = dp[1] = 0;
+        System.out.println(recur(N));
     }
 
-    private static int calculate(int number) {
-        if (number == 1) {
-            return 0;
+    private static int recur(int N) {
+        if (dp[N] == null) {
+            if (N % 6 == 0) {
+                dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
+            } else if (N % 3 == 0) {
+                dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
+            } else if (N % 2 == 0) {
+                dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
+            } else {
+                dp[N] = recur(N - 1) + 1;
+            }
         }
-        if (d[number] > 0) {
-            return d[number];
-        }
-        d[number] = calculate(number - 1) + 1;
-        if (number % 3 == 0) {
-            d[number] = Math.min(d[number], calculate(number / 3) + 1);
-        }
-        if (number % 2 == 0) {
-            d[number] = Math.min(d[number], calculate(number / 2) + 1);
-        }
-        return d[number];
+        return dp[N];
     }
 }
