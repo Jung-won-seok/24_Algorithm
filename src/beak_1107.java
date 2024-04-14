d960d7362254383928fcd1bead94fcce0b3b6922
Jung-won-seok <ukksj0621@naver.com>
2024-04-14 21:24:03 +0900

    리모컨

---
+++ src/beak_1107.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+public class beak_1107 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] error = new int[M];
+        for (int i = 0; i < M; i++) {
+            error[i]= sc.nextInt();
+        }
+
+        for (int a : error) {
+            System.out.println(a);
+        }
+    }
+
+}
