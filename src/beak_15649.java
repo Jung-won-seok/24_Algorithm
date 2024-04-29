75d1e7ef075de2c5d17a609d46f1166d7f3f1380
Jung-won-seok <ukksj0621@naver.com>
2024-04-29 22:24:36 +0900

    N과 M (1)

--- src/beak_15649.java
+++ src/beak_15649.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 public class beak_15649 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        int N = sc.nextInt(); // N = 4
+        int M = sc.nextInt(); // M = 2 -> 개 중에서 2개 중복없이 뽑는 경우
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
+        }
     }
 }
