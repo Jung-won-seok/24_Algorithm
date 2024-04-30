1d7cf6b55409d89c26446dbf7e86c1bfca768888
Jung-won-seok <ukksj0621@naver.com>
2024-04-30 22:49:41 +0900

    N과 M (1)_백트래킹

--- src/beak_15649.java
+++ src/beak_15649.java
@@ -1,14 +1,36 @@
 import java.util.Scanner;
 
 public class beak_15649 {
+
+    public static int[] arr; //
+    public static boolean[] visit; //방문여부 표시
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt(); // N = 4
         int M = sc.nextInt(); // M = 2 -> 개 중에서 2개 중복없이 뽑는 경우
 
-        int[] arr = new int[N];
+        arr = new int[M];
+        visit = new boolean[N];
+        dfs(N, M, 0);
+    }
+
+    private static void dfs(int N, int M, int depth) {
+        if (depth == M) { //끝나는 시점
+            for (int val : arr) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+            return;
+        }
+
         for (int i = 0; i < N; i++) {
-            arr[i] = i + 1;
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(N, M, depth + 1);
+                visit[i] = false;
+            }
         }
     }
 }
