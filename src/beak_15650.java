b240ffbefa0efb3a411a2c6d29d560d5c769966e
Jung-won-seok <ukksj0621@naver.com>
2024-05-20 21:41:10 +0900

    N과 M (2)_백트래킹

---
+++ src/beak_15650.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class beak_15650 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> hash = new HashSet<>();
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+    }
+}
