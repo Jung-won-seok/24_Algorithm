3b38159bfc081adc1660ebb4998b98c23540b3d9
Jung-won-seok <ukksj0621@naver.com>
2024-06-28 21:23:05 +0900

    N과 M (5)

---
+++ src/beak_15654.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class beak_15654 {
+    static public int N, M;
+    static public int[] arr;
+    static public int[] array;
+    static public boolean[] visit;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt(); // N=4
+        M = sc.nextInt(); // M=2
+        array = new int[N]; // array[] = [ 9 8 7 1 ]
+        for (int i = 0; i < N; i++) {
+            array[i] = sc.nextInt();
+        }
+        Arrays.sort(array);
+        arr = new int[M];
+        visit = new boolean[N];
+        dfs(N, M, 0);
+    }
+
+    static public void dfs(int N, int M, int depth) {
+        if (depth == M) {
+            for (int a : arr) {
+                System.out.print(a+" ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = array[i];
+                dfs(N, M, depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
